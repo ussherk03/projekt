6b87353e7ca19179d588c7a2a99fbe2548d1e893
ussherk03@gmail.com <ussherk03@gmail.com>
2020-05-26 01:44:11 +0000

    Updates

---
+++ precache-manifest.6b141892ff92737f27ddd60d04710798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec8548bef3da677dad12b9bf54a829b",
+    "url": "/projekt/index.html"
+  },
+  {
+    "revision": "846c8465515320308fba",
+    "url": "/projekt/static/css/main.42b6d02a.chunk.css"
+  },
+  {
+    "revision": "908e9ac61e7e2ab58104",
+    "url": "/projekt/static/js/2.5723aaab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projekt/static/js/2.5723aaab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "846c8465515320308fba",
+    "url": "/projekt/static/js/main.7ce66133.chunk.js"
+  },
+  {
+    "revision": "f6c823a6c5b7b593fe64",
+    "url": "/projekt/static/js/runtime-main.a2edb591.js"
+  }
+]);
